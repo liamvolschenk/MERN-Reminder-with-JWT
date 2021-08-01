0af20589b5747e34ec0d03efaf51559e75b855de
Liam Volschenk <liamvolschenk@gmail.com>
2021-08-01 13:17:33 +0200

    version 1.0.1

--- server.js
+++ server.js
@@ -20,15 +20,10 @@ app.use(express.urlencoded({
 app.use(routes);
 
 //serve static assets if were in production
-if (process.env.NODE_ENV === 'production') {
-    //set static folder
-    app.use(express.static('client/build'))
-
-    app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-    });
-}
-
+app.use(express.static(path.join(__dirname, 'build')));
+app.get('/*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
 
 db.once('open', () => {
     app.listen(PORT, () => console.log(`Server is running on PORT ${PORT}!`));
